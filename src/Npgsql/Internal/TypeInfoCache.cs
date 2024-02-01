8c57d1ca03716e294d3dc54d938d273a5f424a1b
Nino Floris <mail@ninofloris.com>
2024-02-02 00:58:36 +0100

    Improve boxing info type compatibility check

--- src/Npgsql/Internal/TypeInfoCache.cs
+++ src/Npgsql/Internal/TypeInfoCache.cs
@@ -131,8 +131,11 @@ sealed class TypeInfoCache<TPgTypeId> where TPgTypeId : struct
             if (pgTypeId is not null && info.PgTypeId != pgTypeId)
                 throw new InvalidOperationException("A Postgres type was passed but the resolved PgTypeInfo does not have an equal PgTypeId.");
 
-            if (type is not null && !info.IsBoxing && info.Type != type)
-                throw new InvalidOperationException($"A CLR type '{type}' was passed but the resolved PgTypeInfo does not have an equal Type: {info.Type}.");
+            if (type is not null && info.Type != type)
+            {
+                if (!info.IsBoxing || (info.IsBoxing && !type.IsAssignableFrom(info.Type)))
+                    throw new InvalidOperationException($"A CLR type '{type}' was passed but the resolved PgTypeInfo does not have an equal Type: {info.Type}.");
+            }
 
             return info;
         }
